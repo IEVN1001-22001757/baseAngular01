f3eff8866df659702da6b5681b82a4191113ee20
Adrian Fernandez <79542@alumnos.utleon.edu.mx>
2025-09-18 21:06:42 -0600

    boton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesListComponent } from './heroes/heroes-list/heroes-list.component';
 
 @NgModule({ //DECORADORES
   declarations: [
-    AppComponent //se crea al crear el proyecto
+    AppComponent,
+    HeroesListComponent //se crea al crear el proyecto
   ],
   imports: [
     BrowserModule,
